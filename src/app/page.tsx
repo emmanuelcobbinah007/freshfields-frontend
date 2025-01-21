2e0636b7045947aa6c9ed59a278e67d760937768
Emmanuel Cobbinah <emmanuelcobbinah707@gmail.com>
2025-01-21 11:25:22 +0000

    debugging

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Trust from "./components/Trust";
 import ProductCategories from "./components/ProductCategories";
--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,4 +1,3 @@
-import Navbar from "../components/Navbar";
 import SignInForm from "../components/SignInForm";
 
 export default function SignIn()  {
