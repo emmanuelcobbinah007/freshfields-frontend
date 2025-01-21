d00398074131567ada20b0ab6a69aa0027d926e6
Emmanuel Cobbinah <emmanuelcobbinah707@gmail.com>
2025-01-21 11:04:09 +0000

    working on responsiveness

--- src/app/components/ProductCategories.tsx
+++ src/app/components/ProductCategories.tsx
@@ -11,11 +11,11 @@ import { StaticImageData } from 'next/image'
 
 const ProductCategories = () => {
   return (
-    <div className='w-[80%] mx-auto pt-10'>
+    <div className='w-[90%] md:w-[80%] mx-auto pt-10'>
         <h1 className='font-semibold text-3xl px-4 text-center'>What We Offer</h1>
-        <div className='grid grid-cols-3 gap-10 py-10'>
-        <div className='flex flex-col items-center p-3 hover:cursor-pointer hover:shadow-lg duration-300 rounded-lg hover:scale-105'>
-                <img src={(meatImg as StaticImageData).src} className='w-[75%]' alt='fish' />
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10 py-10'>
+            <div className='flex flex-col items-center p-3 hover:cursor-pointer hover:shadow-lg duration-300 rounded-lg hover:scale-105'>
+                <img src={(meatImg as StaticImageData).src} className='w-[75%]' alt='meat' />
                 <h1 className='text-xl font-semibold text-center mt-5'>Fresh Meat</h1>   
             </div>
             <div className='flex flex-col items-center p-3 hover:cursor-pointer hover:shadow-lg duration-300 rounded-lg hover:scale-105'>
@@ -23,30 +23,29 @@ const ProductCategories = () => {
                 <h1 className='text-xl font-semibold text-center mt-5'>Fresh Fish</h1>   
             </div>
             <div className='flex flex-col items-center p-3 hover:cursor-pointer hover:shadow-lg duration-300 rounded-lg hover:scale-105'>
-                <img src={(oilImg as StaticImageData).src} className='w-[75%]' alt='fish' />
-                <h1 className='text-xl font-semibold text-center mt-5'>Oil</h1>
+                <img src={(fruitImg as StaticImageData).src} className='w-[75%]' alt='fruit' />
+                <h1 className='text-xl font-semibold text-center mt-5'>Fresh Fruits</h1>   
             </div>
             <div className='flex flex-col items-center p-3 hover:cursor-pointer hover:shadow-lg duration-300 rounded-lg hover:scale-105'>
-                <img src={(fruitImg as StaticImageData).src} className='w-[75%]' alt='fish' />
-                <h1 className='text-xl font-semibold text-center mt-5'>Fruits</h1>   
+                <img src={(herbImg as StaticImageData).src} className='w-[75%]' alt='herb' />
+                <h1 className='text-xl font-semibold text-center mt-5'>Fresh Herbs</h1>   
             </div>
             <div className='flex flex-col items-center p-3 hover:cursor-pointer hover:shadow-lg duration-300 rounded-lg hover:scale-105'>
-                <img src={(veggiesImg as StaticImageData).src} className='w-[75%]' alt='fish' />
-                <h1 className='text-xl font-semibold text-center mt-5'>Vegetables</h1>   
+                <img src={(legumeImg as StaticImageData).src} className='w-[75%]' alt='legume' />
+                <h1 className='text-xl font-semibold text-center mt-5'>Fresh Legumes</h1>   
             </div>
             <div className='flex flex-col items-center p-3 hover:cursor-pointer hover:shadow-lg duration-300 rounded-lg hover:scale-105'>
-                <img src={(legumeImg as StaticImageData).src} className='w-[75%]' alt='fish' />
-                <h1 className='text-xl font-semibold text-center mt-5'>Legumes, Grains and Cereals</h1>   
+                <img src={(oilImg as StaticImageData).src} className='w-[75%]' alt='oil' />
+                <h1 className='text-xl font-semibold text-center mt-5'>Fresh Oil</h1>   
             </div>
             <div className='flex flex-col items-center p-3 hover:cursor-pointer hover:shadow-lg duration-300 rounded-lg hover:scale-105'>
-                <img src={(tuberImg as StaticImageData).src} className='w-[75%]' alt='fish' />
-                <h1 className='text-xl font-semibold text-center mt-5'>Tuber</h1>   
+                <img src={(tuberImg as StaticImageData).src} className='w-[75%]' alt='tuber' />
+                <h1 className='text-xl font-semibold text-center mt-5'>Fresh Tubers</h1>   
             </div>
             <div className='flex flex-col items-center p-3 hover:cursor-pointer hover:shadow-lg duration-300 rounded-lg hover:scale-105'>
-                <img src={(herbImg as StaticImageData).src} className='w-[75%]' alt='fish' />
-                <h1 className='text-xl font-semibold text-center mt-5'>Herbs and Spices</h1>   
+                <img src={(veggiesImg as StaticImageData).src} className='w-[75%]' alt='veggies' />
+                <h1 className='text-xl font-semibold text-center mt-5'>Fresh Veggies</h1>   
             </div>
-
         </div>
     </div>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,12 @@
-import Image from "next/image";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Trust from "./components/Trust";
-import About from "./components/About";
 import ProductCategories from "./components/ProductCategories";
 import Footer from "./components/Footer";
 
 export default function Home() {
   return (
     <div className="font-poppins">
-    <div className="bg-[#FFC1070F] h-[40px]"></div>
-    <Navbar/>
     <Hero/>
     <Trust />
     <ProductCategories />
